dbe69be908d346c8d16ada791862501a722879d1
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-05 12:51:13 -0500

    Create the app

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,5 @@
+class EventsController < ApplicationController
+  def index
+    @events = ["BugSmash", "Hackathon", "Kata Camp", "Rails User Group"]
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get "events" => "events#index"
+end
