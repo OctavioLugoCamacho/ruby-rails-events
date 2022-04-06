0fb9962c2b80ec0696225716948964170f0c1bcd
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-06 18:29:26 -0500

    Add the routes and the links to edit a event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
   def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
     @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
+  get "events/:id/edit" => "events#edit", as: "edit_event"
 end
