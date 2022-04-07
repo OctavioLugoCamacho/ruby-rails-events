a4a60c115942930180b513b46564ce556d19d07e
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 15:37:05 -0500

    Add the errors in the form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,8 +13,11 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to @event
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def new
@@ -23,8 +26,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.save
-    redirect_to @event
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def destroy
