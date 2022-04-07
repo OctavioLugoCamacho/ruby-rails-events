3184e6fe5f0c4b76478d013c43bb071bf7574d45
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 12:53:14 -0500

    Add validations in the model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
 
+  validates :name, :description, :location, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG file"
+  }
 
   def self.upcoming
     where("starts_at > ?", Time.now).order("starts_at")
