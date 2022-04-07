f2b06b1c0cdc07b81a21090daf33c76ecd010cc0
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 16:00:51 -0500

    Add the notices and alerts using flash in edit and create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-      redirect_to @event
+      redirect_to @event, notice: "Event successfully updated!"
     else
       render :edit
     end
@@ -27,7 +27,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      redirect_to @event
+      redirect_to @event, notice: "Event successfully created!"
     else
       render :new
     end
