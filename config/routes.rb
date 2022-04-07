1a405e0d9c71f6ea475075ecf3b177da7ef49e3b
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-06 20:11:55 -0500

    Restructure routes with the resources command

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  resources :events
 end
