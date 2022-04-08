bb65934cb1e374e73ddf356165576cf5fc0931a8
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 22:16:02 -0500

    Add the validations for a new registration and the has many in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-
+  has_many :registrations, dependent: :destroy
   validates :name, :description, :location, presence: true
   validates :description, length: { minimum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,15 @@
 class Registration < ApplicationRecord
   belongs_to :event
+  validates :name, presence: true
+  validates :email, format: { with: /\S+@S+/ }
+  HOW_HEARD_OPTIONS = [
+    'Newsletter',
+    'Blog Post',
+    'Twitter',
+    'Web Search',
+    'Friend/Coworker',
+    'Other'
+  ]
+  validates :how_heard,
+    inclusion: { in: HOW_HEARD_OPTIONS }
 end
