9f8fccda5c50db8c83de74938952e2412daa46c3
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-08 12:19:30 -0500

    Add the functions and links to create a new registratiom

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,7 @@
 class Registration < ApplicationRecord
   belongs_to :event
   validates :name, presence: true
-  validates :email, format: { with: /\S+@S+/ }
+  validates :email, format: { with: /\S+@\S+/ }
   HOW_HEARD_OPTIONS = [
     'Newsletter',
     'Blog Post',
