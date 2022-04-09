cd56d0351f509c1455ac1dcf47f9df54386ef438
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-09 15:10:51 -0500

    Add the sign up functions in the controller and the templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   root "events#index"
   resources :events do
     resources :registrations
   end
+  resources :users
+  get "signup" => "users#new"
 end
