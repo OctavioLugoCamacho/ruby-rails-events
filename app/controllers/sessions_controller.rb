ba443464fb6528057c96afbf8ce1c0d61ba40f45
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-11 11:25:23 -0500

    Add the session resources

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    params[:email]
+    params[:password]
+  end
+
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :events do
     resources :registrations
   end
+  resource :session, only: [:new, :create, :destroy]
   resources :users
   get "signup" => "users#new"
 end
