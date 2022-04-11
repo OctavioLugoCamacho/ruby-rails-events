8eb2eb2c0e436ee7853cb6e6c35d50d5f084c557
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-11 12:46:50 -0500

    Create the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
   end
 
   def destroy
-    
+
   end
 end
