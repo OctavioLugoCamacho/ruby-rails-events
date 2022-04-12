22254e940ab644fc7145ea274a8a0b91cd32797a
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-12 17:10:18 -0500

    Settings in registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < ApplicationController
+  before_action :require_signin
   before_action :set_event
 
   def index
@@ -11,6 +12,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @event.registrations.new(registration_params)
+    @registration.user = current_user
     if @registration.save
       redirect_to event_registrations_path(@event),
         notice: "Thanks for registering!"
@@ -21,7 +23,7 @@ class RegistrationsController < ApplicationController
 
   private
     def registration_params
-      params.require(:registration).permit(:name, :email, :how_heard)
+      params.require(:registration).permit(:how_heard)
     end
 
     def set_event
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @registrations = @user.registrations
   end
 
   def new
