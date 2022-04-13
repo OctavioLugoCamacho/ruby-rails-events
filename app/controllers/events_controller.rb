c9b1dfcc5867112ca15410db2ccf299804f87bce
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-13 18:01:23 -0500

    Add the checkboxes in the form to update/create event with categories

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -51,6 +51,6 @@ class EventsController < ApplicationController
     def event_params
       params.require(:event).
         permit(:name, :description, :location, :price, :starts_at,
-          :capacity, :image_file_name)
+          :capacity, :image_file_name, category_ids: [])
     end
 end
